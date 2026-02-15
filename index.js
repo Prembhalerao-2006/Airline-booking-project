93d18b5e54caa5a66b35b5d981c1adc0cb52d344
Prembhalerao-2006 <prembhalerao2022@gmail.com>
2026-02-15 14:56:42 +0530

    work on routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const apiRoutes = reuired('./routes');
 
 const { PORT } = require('./src/config');
 
 const app = express();
 
+app.use('/api', apiRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);   
 });
